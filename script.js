bf04a4b32cc3e77dadd6a41d25875039f4420825
v-imv <v.iminov@gmail.com>
2021-11-23 21:49:55 +0000

    fixed grid issue

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const body = document.querySelector('body');
-
 const gridContainer = document.querySelector("#grid-container");
 
-function gridCreation(num){
-let desiredSize = (num*num);
-let boxDimensions = Math.sqrt(250000/desiredSize);
 
-for(i=0; i < desiredSize; ++i){
-    const div = document.createElement('div');
-    div.classList.add('gridBox');
-    div.setAttribute('style', `border: 1px solid rgb(70, 70, 70); height: ${boxDimensions}px; width: ${boxDimensions}px;`);
-    div.addEventListener('mouseover', () => {
-        div.classList.add('add-color');
+let userInput;
+do {
+userInput = prompt("Enter A Number Between 1-100");
+}  while (!(userInput > 0 && userInput < 101));
+if (userInput > 0 && userInput < 101) {
+    gridCreate();
+}
+console.log(userInput);
+
+function gridCreate(){
+for(let i=0; i < userInput; ++i){
+    let parent = document.createElement('div');
+    parent.classList.add('parent');
+    gridContainer.appendChild(parent);
+
+    for(let x=0; x < userInput; ++x){
+    let child = document.createElement('div');
+    child.classList.add('child');
+    child.addEventListener('mouseover', () => {
+        child.classList.add('add-color');
     })
-    gridContainer.appendChild(div);
+    parent.appendChild(child);
+    }
+
 }
 }
-
-gridCreation(32);
-
-const gridButton = document.createElement('button');
-body.appendChild(gridButton);
-gridButton.setAttribute('class', 'buttonStyle');
-gridButton.textContent = 'Clear & Select Grid Size';
-gridButton.addEventListener('click', () => {
-
-})
